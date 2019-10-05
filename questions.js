f362648aae94b5e56a462d49d2dbf2da6b328eeb
Rhinoceros Beetle <goodhewsanchezkids@gmail.com>
2019-10-05 12:23:56 +0100

    checkpoint

--- questions.js
+++ questions.js
@@ -30,48 +30,83 @@ module.exports = [
         question: "what should RB do",
         optionA: "climb the bolder",
         optionB: "hit the bolder whith a hammer",
-        choseAMessage: "RB climbs the bolder BUT HE SLIPS AND DIES!!!!!!!",
-        chooseAGotoQuestion: 3,
+        choseAMessage: "RB climbs the bolder",
+        chooseAGotoQuestion: 5,
         choseBMessage: "RB hits the bolder with a hammer",
-        chooseBGotoQuestion: 4
+        chooseBGotoQuestion: 3
     },
     {
         questionNumber: 2,
         scenario: "RB walks a little until he comes to a river. He takes a sip it tastes good",
         question: "what should RB do",
         optionA: "swim",
-        optionB: "find another way",
-        choseAMessage: "press enter to continue",
-        chooseAGotoQuestion: 5,
-        choseBMessage: "press enter to continue",
-        chooseBGotoQuestion: 6
+        optionB: "find another way around",
+        choseAMessage: "RB swims across",
+        chooseAGotoQuestion: 6,
+        choseBMessage: "RB decides to find another way round",
+        chooseBGotoQuestion: 4
     },
     {
         questionNumber: 3,
-        scenario: "RB hits the bolder with a hammer sudenly, it opens half way. theres a under ground stair case ",
-        question: "should you",
-        optionA: "",
-        optionB: "",
-        choseAMessage: "",
-        chooseAGotoQuestion: 0,
-        choseBMessage: "",
-        chooseBGotoQuestion: 0
+        scenario: " the bolder opens half way. theres a under ground stair case",
+        question: "should he...",
+        optionA: "take the staircase",
+        optionB: "hit the bolder with a hammer again",
+        choseAMessage: "RB takes the staircase",
+        chooseAGotoQuestion: 11,
+        choseBMessage: "RB hits the bolder with a hammer",
+        chooseBGotoQuestion: 9
     },
     {
-        questionNumber: 0,
-        scenario: "",
-        question: "",
-        optionA: "",
-        optionB: "",
-        choseAMessage: "",
-        chooseAGotoQuestion: 0,
-        choseBMessage: "",
-        chooseBGotoQuestion: 0
+        questionNumber: 4,
+        scenario: "RBs realy hungry he bumps into a berry tree",
+        question: "should he...",
+        optionA: "eat one",
+        optionB: "keep walking",
+        choseAMessage: "RB eats one there realy good he puts loads in his backpack",
+        chooseAGotoQuestion: 7,
+        choseBMessage: "RB chooses to keep walking",
+        chooseBGotoQuestion: 8
     },
+    {
+        questionNumber: 5,
+        scenario: "there are some slippy spots RB slips and dies!!!!!!!!!!!!!!!!!!!!!!!!",
+        isEnd: true
+    },
+    {
+        questionNumber: 6,
+        scenario: "in the river there are crocodiles they eat RB alive while he's swimming to the other side!!!!!!!!!!!!!!!!!!!!!",
+        isEnd: true 
+    },
+    {
+        questionNumber: 7,
+        scenario: "RB has to make a choice",
+        question: "should he...",
+        optionA: "stay there for ever",
+        optionB: "keep walking",
+        choseAMessage: "RB chooses to stay there forever",
+        chooseAGotoQuestion: 10,
+        choseBMessage: "RB chooses to keep walking",
+        chooseBGotoQuestion: 11 
+    },
+    {
+        questionNumber: 8,
+        scenario: "RB dies off hunger!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+        isEnd: true
+    },
+    {
+        questionNumber: 9,
+        scenario: "the sound is so loud it kills you!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+        isEnd: true
+    },
+    {
+        questionNumber: 10,
+        scenario: "winter comes RB dies off coldness!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+        isEnd: true
+    },
+    {
+        questionNumber: 11,
+        scenario: "RBs finally home",
+        isEnd: true
+    }
 ]
-
-
-
-
-
-
--- test.js
+++ test.js
@@ -8,24 +8,34 @@ const questionMaster = function(questions, questionNumber) {
     console.clear()
 
     console.log(question.scenario) 
-    console.log(question.question)
-    console.log("  a) " + question.optionA)
-    console.log("  b) " + question.optionB)
 
-    var answer = readlineSync.question();
-
-    if (answer == "a") {
-        console.log(question.choseAMessage)
-        readlineSync.question("Press enter to continue");
-        questionMaster(questions, question.chooseAGotoQuestion)        
-    } else if (answer == "b") {
-        console.log(question.choseBMessage)
-        readlineSync.question("Press enter to continue");
-        questionMaster(questions, question.chooseBGotoQuestion)                
+    if (question.isEnd) {
+        console.log();
+        console.log('THE END');
     } else {
-        console.log("You've killed RB by not making a choice!")
-    }
 
+        console.log(question.question)
+        console.log("  a) " + question.optionA)
+        console.log("  b) " + question.optionB)
+
+        var answer = readlineSync.question();
+
+        if (answer == "a") {
+            console.log()
+            console.log(question.choseAMessage)
+            console.log()
+            readlineSync.question("Press enter to continue");
+            questionMaster(questions, question.chooseAGotoQuestion)        
+        } else if (answer == "b") {
+            console.log()
+            console.log(question.choseBMessage)
+            console.log()
+            readlineSync.question("Press enter to continue");
+            questionMaster(questions, question.chooseBGotoQuestion)                
+        } else {
+            console.log("You've killed RB by not making a choice!")
+        }
+    }
 }
 
 questionMaster(questions, 0)
