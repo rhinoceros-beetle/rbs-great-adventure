be2c7a0e27ff967aa65b31e25441b92ded7602e6
Rhinoceros Beetle <goodhewsanchezkids@gmail.com>
2019-10-06 12:07:22 +0100

    fixed spacing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const readlineSync = require('readline-sync');
 const questions = require('./questions.js')
-
+const chalk = require('chalk')
+const hungry = require('./hungry.js')
 const house = require('./house.js')
 const rip = require('./rip.js')
 const croc = require('./croc.js')
 const snowflake = require('./snowflake.js')
 const bang = require('./bang.js')
+
 const questionMaster = function(questions, questionNumber) {
 
     var question = questions[questionNumber]
@@ -13,9 +15,9 @@ const questionMaster = function(questions, questionNumber) {
     console.clear()
 
     console.log(question.scenario) 
+    console.log();
 
     if (question.isEnd) {
-        console.log();
         if (question.picture == "croc") {
             croc()
             console.log()
@@ -24,7 +26,7 @@ const questionMaster = function(questions, questionNumber) {
                 bang() 
 
          } else if (question.picture == "snowflake") {
-            snowflakes()
+            snowflake()
 
         } else if (question.picture == "rip") {
             rip()
@@ -32,10 +34,14 @@ const questionMaster = function(questions, questionNumber) {
         } else if (question.picture == "house") {
             house()
 
+        } else if (question.picture == "hungry") {
+            hungry() 
+
         }console.log('THE END');
     } else {
 
         console.log(question.question)
+        console.log()
         console.log("  a) " + question.optionA)
         console.log("  b) " + question.optionB)
 
@@ -45,16 +51,16 @@ const questionMaster = function(questions, questionNumber) {
             console.log()
             console.log(question.choseAMessage)
             console.log()
-            readlineSync.question("Press enter to continue");
+            readlineSync.question(chalk.gray("Press enter to continue"));
             questionMaster(questions, question.chooseAGotoQuestion)        
         } else if (answer == "b") {
             console.log()
             console.log(question.choseBMessage)
             console.log()
-            readlineSync.question("Press enter to continue");
+            readlineSync.question(chalk.gray("Press enter to continue"));
             questionMaster(questions, question.chooseBGotoQuestion)                
         } else {
-            console.log("You've killed RB by not making a choice!")
+            questionMaster(questions, questionNumber)
         }
     }
 }
--- questions.js
+++ questions.js
@@ -15,8 +15,8 @@
 module.exports = [
     {
         questionNumber: 0,
-        scenario: "RB wandering in the wildernes eventualy coming to a menesing fork in the path",
-        question: "Wich direction should he take",
+        scenario: "RB wandered in the wildernes, eventualy coming to a menesing fork in the path.",
+        question: "Wich direction should he take?",
         optionA: "Right",
         optionB: "Left",
         choseAMessage: "RB chooses the path to the Right",
@@ -26,8 +26,8 @@ module.exports = [
     },
     {
         questionNumber: 1,
-        scenario: "After ten minutes of walking RB saw a gargantuan bolder blocking his way",
-        question: "What should RB do",
+        scenario: "After ten minutes of walking RB saw a gargantuan bolder blocking his way.",
+        question: "What should RB do?",
         optionA: "Climb the bolder",
         optionB: "Hit the bolder whith a hammer",
         choseAMessage: "RB climbs the bolder",
@@ -37,8 +37,8 @@ module.exports = [
     },
     {
         questionNumber: 2,
-        scenario: "RB walks a little until he comes to a river. He takes a sip it tastes good",
-        question: "What should RB do",
+        scenario: "RB walks a little until he comes to a river. He takes a sip, it tastes good.",
+        question: "What should RB do?",
         optionA: "Swim",
         optionB: "Find another way around",
         choseAMessage: "RB swims across",
@@ -48,8 +48,8 @@ module.exports = [
     },
     {
         questionNumber: 3,
-        scenario: " The bolder opens half way. theres a under ground stair case",
-        question: "hould he...",
+        scenario: " The bolder opens half way. theres a under ground stair case.",
+        question: "Should he...",
         optionA: "Take the staircase",
         optionB: "Hit the bolder with a hammer again",
         choseAMessage: "RB takes the staircase",
@@ -59,10 +59,10 @@ module.exports = [
     },
     {
         questionNumber: 4,
-        scenario: "RBs realy hungry he bumps into a berry tree",
+        scenario: "RBs realy hungry he bumps into a berry tree.",
         question: "Should he...",
         optionA: "Eat one",
-        optionB: "keep walking",
+        optionB: "Keep walking",
         choseAMessage: "RB eats one there realy good he puts loads in his backpack",
         chooseAGotoQuestion: 7,
         choseBMessage: "RB chooses to keep walking",
@@ -70,22 +70,22 @@ module.exports = [
     },
     {
         questionNumber: 5,
-        scenario: "There are some slippy spots RB slips and dies!!!!!!!!!!!!!!!!!!!!!!!!",
+        scenario: "There are some slippy spots on the bolder RB slips and dies!!!!!!!!!!!!!!!!!!!!!!!!",
         isEnd: true,
         picture:"rip"
     },
     {
         questionNumber: 6,
-        scenario: "In the river there are crocodiles they eat RB alive while he's swimming to the other side!!!!!!!!!!!!!!!!!!!!!",
+        scenario: "In the river there are Crocodiles they eat RB alive while he's swimming to the other side!!!!!!!!!!!!!!!!!!!!!",
         isEnd: true,
         picture:"croc" 
     },
     {
         questionNumber: 7,
-        scenario: "RB has to make a choice",
+        scenario: "RB has to make a choice.",
         question: "Should he...",
-        optionA: "Stay there for ever",
-        optionB: "keep walking",
+        optionA: "Stay there by the berry tree's forever",
+        optionB: "Keep walking toward his home town",
         choseAMessage: "RB chooses to stay there forever",
         chooseAGotoQuestion: 10,
         choseBMessage: "RB chooses to keep walking",
@@ -94,23 +94,24 @@ module.exports = [
     {
         questionNumber: 8,
         scenario: "RB dies off hunger!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-        isEnd: true
+        isEnd: true,
+        picture:"hungry"
     },
     {
         questionNumber: 9,
-        scenario: "the sound is so loud it kills you!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+        scenario: "The sound is so loud it kills RB!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
         isEnd: true,
         picture:"bang"
     },
     {
         questionNumber: 10,
-        scenario: "winter comes RB dies off coldness!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+        scenario: "Winter comes RB dies off coldness!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
         isEnd: true,
         picture:"snowflake"
     },
     {
         questionNumber: 11,
-        scenario: "RBs finally home",
+        scenario: "RBs finally home.",
         isEnd: true,
         picture:"house"
     }
